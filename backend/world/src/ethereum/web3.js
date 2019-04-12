1e67215a5502289dbb3668448ff7a05f8fbb5c56
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-12 16:52:12 +0200

    import truffle output into world backend

--- backend/world/src/ethereum/web3.js
+++ backend/world/src/ethereum/web3.js
@@ -1,10 +1,11 @@
 import Web3 from 'web3'
+import fs from 'fs'
 import * as constants from '../constants.js'
 
 const web3 = new Web3(Web3.providers.WebsocketProvider(constants.ETHEREUM_URL))
+const worldJson = JSON.parse(fs.readFileSync('../../../ethereum/build/contracts/World.json'))
 
-// todo: get abi
-const jsonInterface = ['']
+const jsonInterface = worldJson.abi
 const contract = new web3.eth.Contract(jsonInterface)
 
 export const registerListener = (MyEvent, options, callback) => {
