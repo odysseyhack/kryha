59160476b1f1b93038699f0c0e253adfa0d63124
Jesse van Remmerden <jesse.vanremmerden@student.uva.nl>
2019-04-12 14:05:47 +0200

    Contract Drone done

--- backend/ethereum/contracts/Drone.sol
+++ backend/ethereum/contracts/Drone.sol
@@ -8,11 +8,40 @@ contract Drone{
     bytes32[] dna;
     uint fitness;
 
-    constructor(address _WorldContract, address _parent1, address _parent2, bytes32[] _dna, uint _fitness) public {
+    event NewDrone(address indexed drone, address parent1, address parent2, bytes32[] dna);
+    event DroneDies(address indexed drone);
+
+    struct DroneStruct{
+        address parent1;
+        address parent2;
+        bytes32[] dna;
+    }
+    mapping(address => DroneStruct) public idToDrone;
+
+
+    constructor(address _WorldContract) public {
         WorldContract = _WorldContract;
-        parent1 = _parent1;
-        parent2 = _parent2;
-        dna = _dna;
-        fitness = _fitness;
+    }
+
+    function createDrone(address _parent1, address _parent2, bytes32[] _dna) public {
+        idToDrone[msg.sender] = DroneStruct(_parent1, _parent2, _dna);
+        emit NewDrone(msg.sender, _parent1, _parent2, _dna);
+    }
+    
+    function killDrone() public {
+        delete idToDrone[msg.sender];
+        emit DroneDies(msg.sender);
+    }
+
+    function getParent1(address _drone) public view returns(address){
+        return idToDrone[_drone].parent1;
+    }
+
+    function getParent2(address _drone) public view returns(address){
+        return idToDrone[_drone].parent2;
+    }
+
+    function getDna(address _drone) public view returns(bytes32[]){
+        return idToDrone[_drone].dna;
     }
 }
--- backend/ethereum/migrations/2_contract_migrations.js
+++ backend/ethereum/migrations/2_contract_migrations.js
@@ -1,6 +1,9 @@
 const World = artifacts.require("World");
+const Drone = artifacts.require("Drone");
 
 // TODO: Add constants world size.
 module.exports = function(deployer) {
-    deployer.deploy(World, 50, 50);
+    deployer.deploy(World, 50, 50).then(function(){
+        return deployer.deploy(Drone, World.address);
+    });
 };
