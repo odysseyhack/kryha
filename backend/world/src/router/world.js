def1c15d8569210a7a0cac5da9b4d4eff10892e9
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-12 16:29:37 +0200

    add list points endpoint

--- backend/world/src/router/world.js
+++ backend/world/src/router/world.js
@@ -3,7 +3,7 @@ import Point from '../mongoose/point.js'
 
 const router = express.Router()
 
-router.use(async (req, res, next) => {
+const injectPoint = async (req, res, next) => {
   try {
     const { x, y } = req.body
     let point = await Point.findOne({ x, y }).exec()
@@ -13,19 +13,25 @@ router.use(async (req, res, next) => {
   } catch (error) {
     next(error)
   }
-})
+}
 
-router.get('/point', async (req, res, next) => {
+router.get('/point', injectPoint, async (req, res, next) => {
   return res.status(200).json(res.locals.point)
 })
 
-router.put('/point', async (req, res, next) => {
+router.put('/point', injectPoint, async (req, res, next) => {
   try {
-    const point = res.locals.point
-    return res.status(200).json(await point.updateOne(req.body).exec())
+    const update = await res.locals.point.updateOne(req.body).exec()
+    return res.status(200).json(update)
   } catch (error) {
     next(error)
   }
+})
+
+// todo: list of points
+router.get('/', async (req, res, next) => {
+  const points = await Point.find().exec()
+  return res.status(200).json(points)
 })
 
 export default router
