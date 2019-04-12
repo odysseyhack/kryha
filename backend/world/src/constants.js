a6a1c047f7db577b8aacbfc75c2b264b7cd33131
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-12 16:01:07 +0200

    add ganache

--- backend/world/src/constants.js
+++ backend/world/src/constants.js
@@ -1,2 +1,3 @@
 export const API_PORT = process.env.API_PORT || 9001
 export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/world'
+export const ETHEREUM_URL = process.env.ETHEREUM_URL || 'ws://localhost:8545'
