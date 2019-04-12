35609cf96272b405d659802b06d855c1168d47fa
Max Bos <maxn.bos@gmail.com>
2019-04-12 21:56:14 +0200

    Initial working globe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import Universe from './scenes/universe';
 import Logo from './components/logo';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <React.Fragment>
         <Logo />
         <Universe />
-      </div>
+      </React.Fragment>
     );
   }
 }
