a54a4fe453e8feac26f22b02781d1ad5e3e00d7e
MangoDream1 <axel.verheul@gmail.com>
2019-04-12 15:54:56 +0200

    basic genetics logic

--- backend/drone/index.js
+++ backend/drone/index.js
@@ -1,12 +1,35 @@
-const register = require('./k8s/register')
+const express = require('express')
+const app = express()
 
+const register = require('./k8s/register')
 const eth = require('./helper/eth')
+const GeneticsC = require('./genetics')
+
+const PORT = process.env.PORT || 3000
 
 async function main () {
   let account = await eth.newAccount()
 
   // Register on k8s
   register(account.address)
+
+  const Genetics = new GeneticsC(account.address.slice(1), account)
+
+  let geneticRoutes = require('./genetics/routes')(Genetics)
+
+  app.use('/genetic/', geneticRoutes)
+
+  app.get('/health', (req, res) => {
+    res.send('Hello, World!')
+  })
+
+  app.listen(PORT, (err) => {
+    if (err) {
+      console.err(err.stack)
+    } else {
+      console.log(`App listening on port ${PORT} [${process.env.NODE_ENV} mode]`)
+    }
+  })
 }
 
 main()
