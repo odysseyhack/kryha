09c651901bb278f3869b58b51016b322aae0d7a6
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-13 16:21:58 +0200

    reset to 100 world size

--- backend/world/src/constants.js
+++ backend/world/src/constants.js
@@ -2,4 +2,4 @@ export const API_PORT = process.env.API_PORT || 9001
 export const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/world'
 export const ETHEREUM_URL = process.env.ETHEREUM_URL || 'ws://localhost:8545'
 export const CONTRACTS_URL = process.env.CONTRACTS_URL || 'http://localhost:9002'
-export const WORLD_SIZE = process.env.WORLD_SIZE || 200
+export const WORLD_SIZE = process.env.WORLD_SIZE || 100
