3c801ab2a1221e956de3eba012e7fd7332ef43d4
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-13 14:14:18 +0200

    add drone position to model and update position on world events

--- backend/world/src/ethereum/web3.js
+++ backend/world/src/ethereum/web3.js
@@ -7,8 +7,10 @@ import Drone from '../mongoose/drone.js'
 const web3 = new Web3(Web3.providers.WebsocketProvider(constants.ETHEREUM_URL))
 
 export const registerListener = (contract, MyEvent, options, callback) => {
+  const eventEmitter = contract.events[MyEvent](options)
+  eventEmitter.on('data', callback)
   console.info(`added ${MyEvent} listener`)
-  return contract.events[MyEvent](options, callback)
+  return eventEmitter
 }
 
 export async function setup () {
@@ -18,43 +20,37 @@ export async function setup () {
   const droneContract = new web3.eth.Contract(droneJsonInterface.abi, droneJsonInterface.address)
 
   // got mine resources event, update point in mongo
-  registerListener(worldContract, 'E_MineResources', {}, async (error, event) => {
-    if (error) {
-      console.error(error)
-    } else {
-    // parse event values
-      const { x, y, air, resources, nature, water } = event.returnValues
-      // find point
-      const point = await Point.findOne({ x, y }).exec()
-      // update point
-      point.set('air', Number(point.air) + Number(air))
-      point.set('resources', Number(point.resources) + Number(resources))
-      point.set('nature', Number(point.nature) + Number(nature))
-      point.set('water', Number(point.water) + Number(water))
-      await point.save()
-    }
+  registerListener(worldContract, 'E_MineResources', {}, async (event) => {
+    const { drone: address, x, y, air, resources, nature, water } = event.returnValues
+    // find point
+    const point = await Point.findOne({ x, y }).exec()
+    // update point
+    point.set('air', Number(point.air) + Number(air))
+    point.set('resources', Number(point.resources) + Number(resources))
+    point.set('nature', Number(point.nature) + Number(nature))
+    point.set('water', Number(point.water) + Number(water))
+    await point.save()
+    console.info(`updated point ${x},${y}`)
+    // todo: update drone involved
+    const drone = await Drone.findOne({ address }).exec()
+    drone.set('x', x)
+    drone.set('y', y)
+    await drone.save()
+    console.info(`updated drone ${address}`)
   })
 
-  registerListener(droneContract, 'NewDrone', {}, async (error, event) => {
-    if (error) {
-      console.error(error)
-    } else {
-      const { drone: address, parent1, parent2, dna } = event.returnValues
-      await new Drone({ address, parent1, parent2, dna, fitness: 0 }).save()
-      console.info(`drone ${address} added`)
-    }
+  registerListener(droneContract, 'NewDrone', {}, async (event) => {
+    const { drone: address, parent1, parent2, dna } = event.returnValues
+    await new Drone({ address, parent1, parent2, dna, fitness: 0 }).save()
+    console.info(`drone ${address} added`)
   })
 
-  registerListener(droneContract, 'DroneDies', {}, async (error, event) => {
-    if (error) {
-      console.error(error)
-    } else {
-      const { drone: address } = event.returnValues
-      const drone = await Drone.findOne({ address }).exec()
-      drone.set('alive', false)
-      await drone.save()
-      console.info(`drone ${address} died`)
-    }
+  registerListener(droneContract, 'DroneDies', {}, async (event) => {
+    const { drone: address } = event.returnValues
+    const drone = await Drone.findOne({ address }).exec()
+    drone.set('alive', false)
+    await drone.save()
+    console.info(`drone ${address} died`)
   })
 }
 
