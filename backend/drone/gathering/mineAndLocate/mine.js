58d955410d485d587b1c5f7f84ee175f32174db9
Jesse van Remmerden <jesse.vanremmerden@student.uva.nl>
2019-04-14 00:14:32 +0200

    Mine ceil function added

--- backend/drone/gathering/mineAndLocate/mine.js
+++ backend/drone/gathering/mineAndLocate/mine.js
@@ -12,7 +12,7 @@ async function mine (store) {
   let dnaNum = getDnaNumbers(store.DNA)
   const funcs = functions.array
   for (let i = 0; i < funcs.length; i++) {
-    await store.eth.mineResources(store.x, store.y, funcs[i].air * dnaNum[i], funcs[i].resources * dnaNum[i], funcs[i].nature * dnaNum[i], funcs[i].water * dnaNum[i])
+    await store.eth.mineResources(store.x, store.y, Math.ceil(funcs[i].air * dnaNum[i]), Math.ceil(funcs[i].resources * dnaNum[i]), Math.ceil(funcs[i].nature * dnaNum[i]), Math.ceil(funcs[i].water * dnaNum[i]))
   }
 }
 
