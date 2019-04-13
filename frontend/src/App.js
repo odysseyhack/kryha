52ba71113ed826f154eea7f43ec7ad5c93f8254b
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-13 23:22:42 +0200

    wip drones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import Logo from './components/logo';
 import Controls from './components/controls';
 
 function App() {
-  const [showRegions, setShowRegions] = useState(false);
+  const [showRegions, setShowRegions, showDrones, setShowDrones] = useState(false);
   let universe = useRef();
   return (
     <React.Fragment>
       <Logo />
-      <Controls showRegions={showRegions} setShowRegions={setShowRegions} />
-      <Universe ref={universe} showRegions={showRegions} />
+      <Controls showRegions={showRegions} setShowRegions={setShowRegions} showDrones={showDrones} setShowDrones={setShowDrones} />
+      <Universe ref={universe} showRegions={showRegions} showDrones={showDrones} />
     </React.Fragment>
   );
 }
