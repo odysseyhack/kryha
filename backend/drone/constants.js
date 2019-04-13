e48d4f717ac43d139e05466b2ab7875a8514207a
MangoDream1 <axel.verheul@gmail.com>
2019-04-13 15:36:16 +0200

    downscaled the size

--- backend/drone/constants.js
+++ backend/drone/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  POPSIZE: 100,
-  CHILDREN: 20,
+  POPSIZE: 50,
+  CHILDREN: 10,
   PROCREATE_ATTEMPS: 5,
   TOURNAMENT_ATTACKERS: 3,
   MUTATION_RATE: parseFloat(process.env.MUTATION_RATE) || 0.01,
