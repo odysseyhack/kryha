b7d885eca9e76545dc0514b343701963d82c331c
MangoDream1 <axel.verheul@gmail.com>
2019-04-13 11:43:12 +0200

    added more values to constants

--- backend/drone/constants.js
+++ backend/drone/constants.js
@@ -7,5 +7,10 @@ module.exports = {
   DNA: process.env.DNA || 'DEFAULTDNA',
   PARENT1: process.env.PARENT1 || '0x0000000000000000000000000000000000000000',
   PARENT2: process.env.PARENT2 || '0x0000000000000000000000000000000000000000',
-  WORLD_URL: process.env.WORLD_URL || 'http://world:9001'
+  WORLD_URL: process.env.WORLD_URL || 'http://world:9001',
+  CONTRACTS_URL: process.env.CONTRACTS_URL || 'http://contracts:9002',
+  ETHEREUM_URL: process.env.ETHEREUM_URL || 'http://ganache:8545',
+  TRANSFERRED_AMOUNT: Number(process.env.TRANSFERRED_AMOUNT) || 1000000000,
+  OWNER_PRIVATE_KEY: process.env.OWNER_PRIVATE_KEY || '0x735bf515f3a8fc16aa634574dd4bb2bf2499ea924f461c4f620403a1e45b60fe'
+
 }
