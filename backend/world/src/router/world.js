b00cfed3a329e7af17f1a05c6f3ff41c17665e96
Miguel Lo-A-Foe <miguel.loafoe@gmail.com>
2019-04-13 18:01:03 +0200

    readd x,y to point list

--- backend/world/src/router/world.js
+++ backend/world/src/router/world.js
@@ -30,7 +30,7 @@ router.put('/point', injectPoint, async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   try {
-    const points = await Point.find({}, { air: 1, resources: 1, nature: 1, water: 1, _id: 0 }).sort({ y: 1, x: 1 }).exec()
+    const points = await Point.find({}, { air: 1, resources: 1, nature: 1, water: 1, _id: 0, x: 1, y: 1 }).sort({ y: 1, x: 1 }).exec()
     return res.status(200).json(points)
   } catch (error) {
     next(error)
