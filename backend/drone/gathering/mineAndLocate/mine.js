ad949fc0dfdd00f3c149c6e9cb4faa45a516c2fd
Jesse van Remmerden <jesse.vanremmerden@student.uva.nl>
2019-04-14 10:10:37 +0200

    Mine fix

--- backend/drone/gathering/mineAndLocate/mine.js
+++ backend/drone/gathering/mineAndLocate/mine.js
@@ -11,8 +11,13 @@ function getDnaNumbers (DNA) {
 async function mine (store) {
   let dnaNum = getDnaNumbers(store.DNA)
   const funcs = functions.array
+  let newState = { air: 0, resources: 0, nature: 0, water: 0 }
   for (let i = 0; i < funcs.length; i++) {
-    await store.eth.mineResources(store.x, store.y, Math.ceil(funcs[i].air * dnaNum[i]), Math.ceil(funcs[i].resources * dnaNum[i]), Math.ceil(funcs[i].nature * dnaNum[i]), Math.ceil(funcs[i].water * dnaNum[i]))
+    newState.air += Math.ceil(funcs[i].air * dnaNum[i])
+    newState.resources += Math.ceil(funcs[i].resources * dnaNum[i])
+    newState.nature += Math.ceil(funcs[i].nature * dnaNum[i])
+    newState.water += Math.ceil(funcs[i].water * dnaNum[i])
+    await store.eth.mineResources(store.x, store.y, newState.air, newState.resources, newState.nature, newState.water)
   }
 }
 
